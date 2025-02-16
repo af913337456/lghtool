0dafd2c80312b87002c6c8024b6d9dfcb3b896b5
ghost <linguanhong@blockabc.com>
2025-02-16 18:37:39 +0800

    http

--- simple_http_request.go
+++ simple_http_request.go
@@ -1,13 +1,15 @@
 package LghTool
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
 
-func SimpleHttpGET(urlStr string, data interface{}, setHeader func(header *http.Header)) (err error) {
+func SimpleHttpGET(urlStr string, data interface{}, setHeader func(header *http.Header), getResp func(resp *http.Response)) (err error) {
 	req, err := http.NewRequest(http.MethodGet, urlStr, nil)
 	if err != nil {
 		err = fmt.Errorf("http.NewRequest err: %v\n", err)
@@ -29,16 +31,32 @@ func SimpleHttpGET(urlStr string, data interface{}, setHeader func(header *http.
 	// Do something with the response
 	if resp != nil && resp.Body != nil {
 		defer resp.Body.Close()
-		if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
-			err = fmt.Errorf("json.NewDecoder err: %v\n", err)
-			return
+		if getResp != nil {
+			getResp(resp)
+		}
+		if data != nil {
+			if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
+				err = fmt.Errorf("json.NewDecoder err: %v\n", err)
+				return
+			}
 		}
 	}
 	return
 }
 
-func SimpleHttpPOST(urlStr string, data interface{}, setHeader func(header *http.Header)) (err error) {
-	req, err := http.NewRequest(http.MethodPost, urlStr, nil)
+func SimpleHttpPOST(urlStr string, param interface{}, data interface{}, setHeader func(header *http.Header), getResp func(resp *http.Response)) (err error) {
+
+	var bufferParam io.Reader = nil
+	if param != nil {
+		paramBys, jErr := json.Marshal(param)
+		if jErr != nil {
+			err = fmt.Errorf("json.Marshal(param) err: %v\n", err)
+			return
+		}
+		bufferParam = bytes.NewBuffer(paramBys)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, urlStr, bufferParam)
 	if err != nil {
 		err = fmt.Errorf("http.NewRequest err: %v\n", err)
 		return
@@ -59,9 +77,14 @@ func SimpleHttpPOST(urlStr string, data interface{}, setHeader func(header *http
 	// Do something with the response
 	if resp != nil && resp.Body != nil {
 		defer resp.Body.Close()
-		if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
-			err = fmt.Errorf("json.NewDecoder err: %v\n", err)
-			return
+		if getResp != nil {
+			getResp(resp)
+		}
+		if data != nil {
+			if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
+				err = fmt.Errorf("json.NewDecoder err: %v\n", err)
+				return
+			}
 		}
 	}
 	return
--- tool_test.go
+++ tool_test.go
@@ -8,3 +8,7 @@ import (
 func Test_GetPreday(t *testing.T) {
 	fmt.Println(GetPreday("2007-01-02 15:04:05"))
 }
+
+func Test_SimplePOST(t *testing.T) {
+
+}
